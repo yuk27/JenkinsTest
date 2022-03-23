5b966f243bdfafb0f56b2af43ee99620dfa10e6a
yuk2794@gmail.com <25yB$!E*@vhG&RDDto0>
2022-03-22 19:54:10 -0600

    Add Main

--- Main.py
+++ Main.py
@@ -1,6 +1,10 @@
-print("Hello World!")
+import datetime
+import time
 
 with open("file.txt", 'w') as f:
-    f.write("Hello World!")
+    f.write("Today is {0}!".format(datetime.date.today()))
+f.close()
 
+with open("file.txt", 'r') as f:
+    print(f.read())
 f.close()
